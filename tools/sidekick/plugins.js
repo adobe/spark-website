a9a58212cd30a0b6aa9deb23ef56930da4253973
rofe <github@rofe.com>
2021-04-16 17:19:09 +0200

    fix(sidekick): reload button

--- tools/sidekick/plugins.js
+++ tools/sidekick/plugins.js
@@ -35,7 +35,7 @@
   sk.add({
     id: 'reload',
     override: true,
-    condition: (sidekick) => sidekick.config.innerHost.endsWith(sidekick.location.innerHost) || sidekick.location.hostname === 'localhost',
+    condition: (sidekick) => sidekick.config.innerHost.endsWith(sidekick.location.hostname) || sidekick.location.hostname === 'localhost',
     button: {
       action: () => {
         const { location } = sk;
