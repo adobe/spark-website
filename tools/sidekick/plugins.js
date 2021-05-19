09a175d5d161ebf06103043d45d36c6dae329625
rofe <github@rofe.com>
2021-05-19 11:45:06 +0200

    feat(sidekick): use hlx3 edit/preview lookup

--- tools/sidekick/plugins.js
+++ tools/sidekick/plugins.js
@@ -9,7 +9,7 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-/* global window fetch */
+/* global window fetch btoa */
 
 // This file contains the spark-specific plugins for the sidekick.
 (() => {
@@ -17,6 +17,10 @@
   if (typeof sk !== 'object') return;
 
   // HLX3 --------------------------------------------------------------------------
+
+  function getAdminUrl({ owner, repo, ref }, path) {
+    return `https://admin.hlx3.page/${owner}/${repo}/${ref}${path}`;
+  }
 
   async function hlx3Publish(config, location, path) {
     if (!config.host
@@ -77,6 +81,56 @@
           }
         } else {
           sk.notify('Successfully published');
+        }
+      },
+    },
+  });
+
+  // override edit button
+  sk.add({
+    override: true,
+    id: 'edit',
+    condition: (sidekick) => sidekick.isHelix(),
+    button: {
+      action: (evt) => {
+        const { config, location } = sk;
+        const path = location.pathname;
+        const file = path.split('/').pop() || 'index'; // use 'index' if no filename
+        let editPath;
+        if (file.endsWith('.html')) {
+          editPath = path.replace(/\.html$/, '.lnk');
+        } else if (!file.includes('.')) {
+          editPath = `${path.endsWith(file) ? path : `${path}${file}`}.lnk`;
+        }
+        const url = getAdminUrl(config, editPath);
+        if (evt.metaKey || evt.which === 2) {
+          window.open(url);
+        } else {
+          window.location.href = url;
+        }
+      },
+    },
+  });
+
+  // override preview button
+  sk.add({
+    override: true,
+    id: 'preview',
+    condition: (sidekick) => sidekick.isEditor() || sidekick.isOuter(),
+    button: {
+      action: (evt) => {
+        const { config, location } = sk;
+        let url;
+        if (sk.isEditor()) {
+          url = getAdminUrl(config, `/hlx_${btoa(location.href).replace(/\+/, '-').replace(/\//, '_')}.lnk`);
+        } else {
+          const host = location.host === config.innerHost ? config.host : config.innerHost;
+          url = `https://${host}${location.pathname}`;
+        }
+        if (evt.metaKey || evt.which === 2) {
+          window.open(url);
+        } else {
+          window.location.href = url;
         }
       },
     },
