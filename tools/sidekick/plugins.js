76533fa598f8d0105134bb91b85f3cb315605178
rofe <github@rofe.com>
2021-06-17 08:12:44 +0200

    chore: remove unused global

--- tools/sidekick/plugins.js
+++ tools/sidekick/plugins.js
@@ -9,7 +9,7 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-/* global window fetch */
+/* global window */
 
 // This file contains the spark-specific plugins for the sidekick.
 (() => {
